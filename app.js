585699f36e159cb34b32346340b44c1923c8019c
jadsonfe <jadson.a.f.s.l@gmail.com>
2024-07-10 17:46:37 -0300

    Cadastro do usuario feito

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const app = express()
 // Config JSON response 
 app.use(express.json())
 
+// Models
+const User = require('./models/User')
+
 // open Route - Public Route
 app.get('/', (req, res) =>(
     res.status(200).json({msg: 'bem vindo a nossa API'})
@@ -25,8 +28,52 @@ app.post('/auth/register', async(req,res) => {
     if(!name){
         return res.status(422).json({msg: 'O nome é obrigatorio!'})
     }
+    if(!email){
+        return res.status(422).json({msg: 'O email é obrigatorio!'})
+    }
+    if(!password){
+        return res.status(422).json({msg: 'A senha é obrigatoria!'})
+    }
+
+    if(password !== confirmpassword){
+        return res.status(422).json({msg: 'As senhas não conferem'})
+    }
+
+    // checar se o usuario existe
+    const userExists = await User.findOne({ email: email})
+
+    if(userExists){
+        return res.status(422).json({msg: 'Usuario já existente'})
+    }
+
+    // create password
+const salt = await bcrypt.genSalt(12)
+const passwordHash = await bcrypt.hash(password, salt)
+
+// Create user
+const user = new User({
+    name,
+    email,
+    password,
+})
+
+try {
+
+    await user.save()
+
+    res.status(201).json({msg: 'Usuario criado com sucesso!'})
+
+}catch(error) {
+    console.log(error)
+    res.status(500).json({msg: "Aconteceu um erro no servidor, tente novamente mais tarde",
+
+        })
+
+}
 
 })
+
+
 
 // Credencials
 const dbUser = process.env.DB_USER
