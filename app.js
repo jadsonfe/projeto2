86eaaa090933ed5a4d745d2e1b9d831c0eb6f851
jadsonfe <jadson.a.f.s.l@gmail.com>
2024-07-10 18:52:15 -0300

    sistema de login

--- app.js
+++ app.js
@@ -18,6 +18,43 @@ app.get('/', (req, res) =>(
     res.status(200).json({msg: 'bem vindo a nossa API'})
 ))
 
+// Private Route
+app.get('/user/:id', checkToken, async (req, res) => {
+
+    const id = req.params.id
+
+    // checando se o usuario existe
+    const user = await User.findById(id, '-password')
+
+    if(!user){
+        return res.status(404).json({msg: 'Usuario não encontrado'})
+    }
+
+    res.status(200).json({user})
+
+})
+
+function checkToken(req, res, next) {
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({msg:"Acesso negado"})
+    }
+
+    try {
+
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+
+    } catch(error) {
+        res.status(400).json({msg: "Token invalido"})
+    }
+}
+
 // Register User
 
 app.post('/auth/register', async(req,res) => {
@@ -54,7 +91,7 @@ const passwordHash = await bcrypt.hash(password, salt)
 const user = new User({
     name,
     email,
-    password,
+    password: passwordHash,
 })
 
 try {
@@ -73,7 +110,56 @@ try {
 
 })
 
+// Login User 
+app.post("/auth/login", async (req, res) => {
 
+    const {email,password} = req.body
+
+    // validations
+    if(!email){
+        return res.status(422).json({msg: 'O email é obrigatorio!'})
+    }
+    if(!password){
+        return res.status(422).json({msg: 'A senha é obrigatoria!'})
+    }
+
+    // checar se o usario existe
+    const user = await User.findOne({email: email})
+
+    if(!user){
+        return res.status(404).json({msg: 'Usuario não encontrado!'})
+    }
+
+    // checando se a senha combina
+    const checkPassword = await bcrypt.compare(password, user.password)
+
+    if(!checkPassword){
+        return res.status(422).json({msg: 'Senha errada'})
+    }
+
+    try { 
+
+        const secret = process.env.SECRET
+        
+        const token = jwt.sign(
+            {
+            id: user._id,
+        },
+            secret,
+        )
+
+        res.status(200).json({msg: 'Autenticação realizada com sucesso!', token})
+
+    }catch(err) {
+        console.log(error)
+
+        res.status(500).json({
+            msg:'Aconteceu um erro no servidor, tente novamente mais tarde',
+        })
+
+    }
+
+})
 
 // Credencials
 const dbUser = process.env.DB_USER
